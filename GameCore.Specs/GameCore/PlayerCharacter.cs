7b24bb87d3684ff8b94b0e81bd7d98fc411aa176
Darrius Wright <darriuswrightgd@gmail.com>
2016-05-17 13:15:55 -0600

    Specflow start

---
+++ GameCore.Specs/GameCore/PlayerCharacter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCore
+{
+    public class PlayerCharacter
+    {
+        public void Hit(int damage)
+        {
+            Health -= damage;
+            if (Health <= 0)
+            {
+                IsDead = true;
+            }
+        }
+
+        public bool IsDead { get; private set; }
+
+        public int Health { get; private set; } = 100;
+    }
+}
